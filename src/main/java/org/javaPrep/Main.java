dfdd86400d19104da3530c38c507c8650e1e7c97
Chaitanya Naralasetti <chaitanya-Naralasetti>
2025-11-03 09:31:20 +0530

    Revert "Added static logic of adding two numbers and printing them in console."
    This reverts commit fd68141a7ad6eb38198b64d210ca8738afba79bb.

--- src/main/java/org/javaPrep/Main.java
+++ src/main/java/org/javaPrep/Main.java
@@ -3,8 +3,5 @@ package org.javaPrep;
 public class Main {
     static void main() {
         System.out.println("Java Prep 2025 Nov Begin..!");
-        int a = 5;
-        int b = 5;
-        System.out.println("Sum of a and b: " + (a+b));
     }
 }
