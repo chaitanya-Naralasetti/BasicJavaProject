fd68141a7ad6eb38198b64d210ca8738afba79bb
Chaitanya Naralasetti <chaitanya-Naralasetti>
2025-11-03 09:17:08 +0530

    Added static logic of adding two numbers and printing them in console.

--- src/main/java/org/javaPrep/Main.java
+++ src/main/java/org/javaPrep/Main.java
@@ -3,5 +3,8 @@ package org.javaPrep;
 public class Main {
     static void main() {
         System.out.println("Java Prep 2025 Nov Begin..!");
+        int a = 5;
+        int b = 5;
+        System.out.println("Sum of a and b: " + (a+b));
     }
 }
