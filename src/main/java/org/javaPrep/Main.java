c89c8794d3ab927e3d4c1316ef16c2b8844a3b52
Chaitanya Naralasetti <chaitanya-Naralasetti>
2025-11-04 01:20:19 +0530

    Sum of two Numbers

--- src/main/java/org/javaPrep/Main.java
+++ src/main/java/org/javaPrep/Main.java
@@ -3,5 +3,8 @@ package org.javaPrep;
 public class Main {
     static void main() {
         System.out.println("Java Prep 2025 Nov Begin..!");
+        Integer int1 = 1;
+        Integer int2 = 2;
+        System.out.printf("Sum of two numbers %d and %d is: " + (int1 + int2), int1, int2);
     }
 }
