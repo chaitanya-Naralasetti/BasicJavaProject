3ad9ad25716726cb1af622e98c1ff91431dcc2e4
Chaitanya Naralasetti <chaitanya-Naralasetti>
2025-11-04 10:34:57 +0530

    Login Feature Development in progress

--- src/main/java/org/javaPrep/Main.java
+++ src/main/java/org/javaPrep/Main.java
@@ -2,9 +2,7 @@ package org.javaPrep;
 
 public class Main {
     static void main() {
-        System.out.println("Java Prep 2025 Nov Begin..!");
-        Integer int1 = 15;
-        Integer int2 = 25;
-        System.out.printf("Sum of two numbers %d and %d is: " + (int1 + int2), int1, int2);
+        System.out.println("Java Prep 2025 Nov Day-2..!");
+        System.out.println("Feature: Login implementation Started..!!");
     }
 }
